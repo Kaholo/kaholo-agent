192fad0a7a8711e6cce5f2c5a08e7b26e03e10be
Yehonatan Bitton <bittonye@post.bgu.ac.il>
2015-10-07 08:12:17 +0300

    file server support

--- production-map-base-agent/api/controllers/TaskController.js
+++ production-map-base-agent/api/controllers/TaskController.js
@@ -5,7 +5,8 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 var request = require('request');
-var DEDICATED_AGENT_URL = "http://localhost:8100/task/register";
+var DEDICATED_AGENT_URL_CLI = "http://localhost:8100/task/register";
+var DEDICATED_AGENT_URL_FTP = "http://localhost:8100/task/registerFtp";
 module.exports = {
 	register: function (req, res) {
 		var execution_result = {msg: "registered task!"};
@@ -13,7 +14,22 @@ module.exports = {
 		console.log("Got Task");
 		console.log(operation);
 		request.post(
-		    DEDICATED_AGENT_URL,
+		    DEDICATED_AGENT_URL_CLI,
+		    { form: operation },
+		    function (error, response, body) {
+		        if (!error && response.statusCode == 200) {
+		            return res.send(body);
+		        }
+		    }
+		);
+	},
+	registerFtp: function(req, res){
+		var execution_result = {msg: "registered task!"};
+		var operation = req.body;
+		console.log("Got Task");
+		console.log(operation);
+		request.post(
+		    DEDICATED_AGENT_URL_FTP,
 		    { form: operation },
 		    function (error, response, body) {
 		        if (!error && response.statusCode == 200) {
