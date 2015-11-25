95854ceec4653c3081397aa87081f1605f7ec5a5
Yehonatan Bitton <bittonye@post.bgu.ac.il>
2015-11-25 04:21:02 +0200

    call each dedicated agent by it's own url

--- production-map-base-agent/api/controllers/TaskController.js
+++ production-map-base-agent/api/controllers/TaskController.js
@@ -5,23 +5,23 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 var request = require('request');
-var DEDICATED_AGENT_URL_CLI = "http://localhost:8100/task/register";
-var DEDICATED_AGENT_URL_FILESERVER = "http://localhost:8100/task/registerfileserver";
+// var DEDICATED_AGENT_URL_CLI = "http://localhost:8100/task/register";
+// var DEDICATED_AGENT_URL_FILESERVER = "http://localhost:8100/task/registerfileserver";
 module.exports = {
 	register: function (req, res) {
 		var execution_result = {msg: "registered task!"};
 		var action = req.body;
 		console.log("Got Task");
 		console.log(action);
-		var DEDICATED_AGENT_URL;
-		if(action.server.type === "CommandLine"){
-			DEDICATED_AGENT_URL = DEDICATED_AGENT_URL_CLI;
-		}
-		else{
-			DEDICATED_AGENT_URL = DEDICATED_AGENT_URL_FILESERVER;
-		}
+		var DEDICATED_AGENT_URL = action.server.url;
+		// if(action.server.type === "CommandLine"){
+		// 	DEDICATED_AGENT_URL = DEDICATED_AGENT_URL_CLI;
+		// }
+		// else{
+		// 	DEDICATED_AGENT_URL = DEDICATED_AGENT_URL_FILESERVER;
+		// }
 		request.post(
-		    DEDICATED_AGENT_URL_CLI,
+		    DEDICATED_AGENT_URL,
 		    { form: action },
 		    function (error, response, body) {
 		        if (!error && response.statusCode == 200) {
