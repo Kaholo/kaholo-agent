9df766616b885bd03d260c82c7e5fcf8604df065
Yehonatan Bitton <bittonye@post.bgu.ac.il>
2015-11-25 23:45:24 +0200

    reverty state for agents

--- production-map-base-agent/api/controllers/TaskController.js
+++ production-map-base-agent/api/controllers/TaskController.js
@@ -5,8 +5,8 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 var request = require('request');
-// var DEDICATED_AGENT_URL_CLI = "http://localhost:8100/task/register";
-// var DEDICATED_AGENT_URL_FILESERVER = "http://localhost:8100/task/registerfileserver";
+var DEDICATED_AGENT_URL_CLI = "http://localhost:8100/task/register";
+var DEDICATED_AGENT_URL_FILESERVER = "http://localhost:8100/task/registerfileserver";
 module.exports = {
 	register: function (req, res) {
 		var execution_result = {msg: "registered task!"};
@@ -14,12 +14,12 @@ module.exports = {
 		console.log("Got Task");
 		console.log(action);
 		var DEDICATED_AGENT_URL = action.server.url;
-		// if(action.server.type === "CommandLine"){
-		// 	DEDICATED_AGENT_URL = DEDICATED_AGENT_URL_CLI;
-		// }
-		// else{
-		// 	DEDICATED_AGENT_URL = DEDICATED_AGENT_URL_FILESERVER;
-		// }
+		if(action.server.type === "CommandLine"){
+			DEDICATED_AGENT_URL = DEDICATED_AGENT_URL_CLI;
+		}
+		else{
+			DEDICATED_AGENT_URL = DEDICATED_AGENT_URL_FILESERVER;
+		}
 		request.post(
 		    DEDICATED_AGENT_URL,
 		    { form: action },
