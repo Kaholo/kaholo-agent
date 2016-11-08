028b19d17f8f6d8b55c3e31132eabb9e592aca0b
Yehonatan Bitton <lolporer@gmail.com>
2016-11-08 09:40:23 +0200

    mysql: add support for running sql file

--- production-map-base-agent/libs/mySQL/app.js
+++ production-map-base-agent/libs/mySQL/app.js
@@ -1,6 +1,7 @@
 var q = require('q');
 var async = require('async');
 var mysql      = require('mysql');
+var fs = require('fs');
 
 function executeQuery(action){
 	var deferred = q.defer();
@@ -24,6 +25,37 @@ function executeQuery(action){
 	return deferred.promise;
 }
 
+function executeSQLFile(action){
+	var deferred = q.defer();
+	var connection = mysql.createConnection({
+	host     : action.params.host,
+	user     : action.params.user,
+	password : action.params.password,
+	database : action.params.db,
+	multipleStatements: true
+	});
+	connection.connect();
+
+	fs.readFile(action.params.filePath, 'utf8', function(err, queries) {
+		if(err){
+			return deferred.resolve({"error": JSON.stringify(err)});
+		}
+
+		connection.query(queries, function(err, results) {
+			if(err){
+				return deferred.resolve({"error": JSON.stringify(err)});
+			}
+			return deferred.resolve({"res": JSON.stringify(results)});
+		});
+
+		connection.end();
+	});
+
+	return deferred.promise;
+}
+
 exports.executeQuery = executeQuery;
+
+exports.executeSQLFile = executeSQLFile;
 
 exports.name = "MySQL";
