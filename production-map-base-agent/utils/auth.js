5bfb214a680499d05ff33fd2357b051c7c5a2457
tallevi12 <tallevi12@gmail.com>
2017-01-16 01:46:49 +0200

    black fix - yoni please fix it when you can

--- production-map-base-agent/utils/auth.js
+++ production-map-base-agent/utils/auth.js
@@ -1,21 +1,9 @@
 var q = require('q');
 var request = require('superagent');
 
-exports.login = function(username, password, server) {
+exports.login = function(server) {
 	var deferred = q.defer();
 	var agent = request.agent();
-	agent
-	.post(server + "/auth/local")
-	.withCredentials()
-	.send({identifier: username, password: password})
-	.set('Accept', 'application/json')
-	.end(function (err, res) {
-		if(err){
-			deferred.reject(res);
-		}
-		else{
-			deferred.resolve({"agent": agent, "res": res});
-		}
-	});
+	deferred.resolve({"agent": agent});
 	return deferred.promise;
 }
