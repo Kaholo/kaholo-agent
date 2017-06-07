b715676b35d77d8013cab1a78f4519954426a0bd
Yehonatan Bitton <lolporer@gmail.com>
2017-06-07 21:50:32 +0300

    log: add winston logger

--- production-map-base-agent/app.js
+++ production-map-base-agent/app.js
@@ -4,6 +4,15 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var winston = require('winston');
+var expressWinston = require('express-winston');
+
+var fs = require('fs');
+var logsdir = './logs';
+
+if (!fs.existsSync(logsdir)){
+    fs.mkdirSync(logsdir);
+}
 
 var routes = require('./routes/index');
 
@@ -21,7 +30,46 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 
+const tsFormat = () => (new Date()).toLocaleTimeString();
+
+app.use(expressWinston.logger({
+  transports: [
+    new winston.transports.Console({
+      json: true,
+      colorize: true
+    }),
+    new (winston.transports.File)({
+      filename: path.join(logsdir, `httprequests.log`),
+      timestamp: tsFormat,
+      level: 'info',
+      json: true
+    })
+  ],
+  meta: true, // optional: control whether you want to log the meta data about the request (default to true)
+  msg: "HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+  expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
+  colorize: true, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
+  ignoreRoute: function (req, res) { return false; } // optional: allows to skip some log messages based on request and/or response
+}));
+
 app.use('/', routes);
+
+app.use(expressWinston.errorLogger({
+  transports: [
+    new (winston.transports.Console)({
+      timestamp: tsFormat,
+      colorize: true,
+      json: true,
+      level: 'error'
+    }),
+    new (winston.transports.File)({
+      filename: path.join(logsdir, 'error.log'),
+      timestamp: tsFormat,
+      level: 'error',
+      json: true
+    })
+  ]
+}));
 
 // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
