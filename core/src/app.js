69068d23d043430631f61352b9298789ff5450a5
Roy Talyosef <roy@productionmap.com>
2018-01-30 13:17:21 +0200

    PMG-329 re arrange agent modules Creating key if needed on agent startup

---
+++ core/src/app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const winston = require('winston');
+const expressWinston = require('express-winston');
+const parseArgs = require('minimist')(process.argv.slice(2));
+
+const keyPath = path.join(path.dirname(path.dirname(__dirname)), 'config', 'key.pm');
+
+if (!fs.existsSync(path)) {
+    const createKey = require("./utils/createKey");
+    createKey.generateKey(keyPath);
+}
+
+const app = express();
+
+const PORT = (parseArgs.PORT || 8090);
+
+app.use(expressWinston.logger({
+    transports: [
+        new winston.transports.Console({
+            json: true,
+            colorize: true
+        })
+    ],
+    meta: true, // optional: control whether you want to log the meta data about the request (default to true)
+    msg: "HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+    expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
+    colorize: false, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
+    ignoreRoute: function (req, res) { return false; } // optional: allows to skip some log messages based on request and/or response
+}));
+
+
+
+
+app.listen(PORT, () => console.log(`Agent listening on port ${PORT}`));
