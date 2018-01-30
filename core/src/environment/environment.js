28baf5f77504695027a010e9f952a1d65505f241
Roy Talyosef <roy@productionmap.com>
2018-01-30 16:22:31 +0200

    PMG-329 re arrange agent modules Update environment

--- core/src/environment/environment.js
+++ core/src/environment/environment.js
@@ -1,7 +1,16 @@
 const path = require("path");
 const config = require("../../../config/config");
+const parseArgs = require('minimist')(process.argv.slice(2));
 
-const keyPath = {keyPath: path.join(path.dirname(path.dirname(path.dirname(__dirname))), 'config', 'key.pm')};
-let env = Object.assign({}, keyPath, config);
+const paths = {
+    keyPath: path.join(path.dirname(path.dirname(path.dirname(__dirname))), 'config', 'key.pm'),
+    pluginsPath: path.join(path.dirname(path.dirname(path.dirname(__dirname))), 'libs', 'plugins')
+};
+
+const server = {
+  server_url: parseArgs.SERVER_URL || 'localhost:3000'
+};
+
+let env = Object.assign({}, paths, config);
 
 module.exports = env;
