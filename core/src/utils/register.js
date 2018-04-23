7205b04bc9c0875637f731630709a5ed42649d38
Roy Talyosef <roy@productionmap.com>
2018-04-23 18:52:24 +0300

    PMG-432 add dynamic agent selection to run on parsing tag and name argv and passing it to server in register.js

--- core/src/utils/register.js
+++ core/src/utils/register.js
@@ -1,43 +1,44 @@
-const os = require("os");
 const winston = require("winston");
 
 const request = require("superagent");
 
+const env = require('../environment/environment');
 
 function sendKeyToServer(agentKey, serverUrl, privateUrl, publicUrl) {
     let agent = request.agent();
     return new Promise((resolve, reject) => {
 
-    agent
-        .post(`${serverUrl}/api/agents/add`)
-        .withCredentials()
-        .send({
-            name: os.hostname().replace(".", "") + '-' + process.platform.replace(".", ""),
-            url: privateUrl,
-            publicUrl: publicUrl,
-            key: agentKey
-        })
-        .set('Accept', 'application/json, text/plain, */*')
-        .set('Content-Type', 'application/json;charset=UTF-8')
-        .end(function (err, res) {
-            if (err) {
-                winston.error(
-                    `Failed connecting to server. Possible reasons are:
+        agent
+            .post(`${serverUrl}/api/agents/add`)
+            .withCredentials()
+            .send({
+                name: env.agentName,
+                attributes:(env.attributes || []),
+                url: privateUrl,
+                publicUrl: publicUrl,
+                key: agentKey
+            })
+            .set('Accept', 'application/json, text/plain, */*')
+            .set('Content-Type', 'application/json;charset=UTF-8')
+            .end(function (err, res) {
+                if (err) {
+                    winston.error(
+                        `Failed connecting to server. Possible reasons are:
                     1. Server url is incorrect
                     2. Server is down
                     3. Agent Key is forbidden for use
                         in this case delete the key folder and rerun the application`);
-                winston.info("Exiting process");
-                /* close program when failed connecting to the server */
-                reject();
-                process.exit();
-            }
-            else {
-                console.log("Agent installed successfully.");
+                    winston.info("Exiting process");
+                    /* close program when failed connecting to the server */
+                    reject();
+                    process.exit();
+                }
+                else {
+                    console.log("Agent installed successfully.");
 
-            }
-            return resolve();
-        });
+                }
+                return resolve();
+            });
     });
 }
 
