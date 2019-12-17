58074e02d1691b0be4b5d93d616094707943ea19
Matan Kadosh <kadosh10001@gmail.com>
2019-12-17 11:11:42 +0100

    * reformatting

--- windows-service.js
+++ windows-service.js
@@ -1,21 +1,24 @@
-var path = require('path');
-var Service = require('node-windows').Service;
+var path = require("path");
+var Service = require("node-windows").Service;
 
-var appPath = path.join(__dirname, 'core/src/app.js');
+var appPath = path.join(__dirname, "core/src/app.js");
 
 // Create a new service object
 var svc = new Service({
-  name:'Kaholo-Agent',
-  description: 'Kaholo-Agent',
+  name: "Kaholo-Agent",
+  description: "Kaholo-Agent",
   script: appPath
 });
 
 // Listen for the "install" event, which indicates the
 // process is available as a service.
-svc.on('install',function(){
-  setTimeout(()=> {
-     svc.start();
-  },1000)
+svc.on("install", function() {
+  console.log("Service installation finished");
+  setTimeout(() => {
+    console.log("Starting Kaholo-Agent service");
+    svc.start();
+  }, 1000);
 });
 
+console.log("Service installation started");
 svc.install();
