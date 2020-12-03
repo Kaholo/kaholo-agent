479e59d524223315b652ffd5f5eefed33b373354
Krzysztof Buchacz <k.buchacz@gmail.com>
2020-12-03 17:49:44 +0100

    Fix paths

--- config/environment.js
+++ config/environment.js
@@ -45,7 +45,10 @@ module.exports = async function(){
     }
 
     process.env.BASE_DIR = BASE_DIR;
-    process.env.PLUGINS_DIR_PATH = path.join(BASE_DIR, 'libs', 'plugins');
+    process.env.PLUGINS_DIR_PATH = 
+        process.env.NODE_ENV === "production" ? path.join(BASE_DIR, '..', 'libs', 'plugins') :  path.join(BASE_DIR, 'libs', 'plugins');
+
+    console.info(process.env.PLUGINS_DIR_PATH);
 
     process.env.AGENT_NAME = process.env.AGENT_NAME || os.hostname().replace(".", "") + '-' + process.platform.replace(".", "");
     
