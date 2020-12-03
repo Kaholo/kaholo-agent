f14421da273ff5bd633b36e10bca597084db807a
arispen <arispen@gmail.com>
2020-12-03 17:40:08 +0100

    startAgent in jest global config

--- tests/helpers/global-setup.js
+++ tests/helpers/global-setup.js
@@ -1,3 +1,4 @@
+const { startAgent } = require("../../app");
 
 module.exports = async () => {
   global.agent = await startAgent();
--- tests/plugins.test.js
+++ tests/plugins.test.js
@@ -2,16 +2,15 @@ const supertest = require("supertest");
 const { configureSuperTest } = require("./helpers/supertest.wrapper");
 
 describe("Plugins tests", () => {
-    let request;
-    beforeEach(() => {
-        request = configureSuperTest(supertest);
-      });
-    describe("POST /", () => {
-        it("should return list of plugins", () => {
-            const { body, statusCode } = await request
-                .post(`/plugins/upload`);
-            expect(statusCode).toBe(200);
-            expect(body).toBeTruthy();
-        });
+  let request;
+  beforeEach(() => {
+    request = configureSuperTest(supertest);
+  });
+  describe("POST /", () => {
+    it("should return list of plugins", async () => {
+      const { body, statusCode } = await request.post(`/plugins/upload`);
+      expect(statusCode).toBe(200);
+      expect(body).toBeTruthy();
     });
+  });
 });
