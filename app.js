75fc1ef6d56320fdc1ee887196de5534cf9fe658
Krzysztof Buchacz <k.buchacz@gmail.com>
2020-12-09 11:04:50 +0100

    Fix asyncs

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const enviormentConfig = require('./config/environment');
 const expressConfig = require('./config/express');
-
 const bootstrap = require("./utils/bootstrap");
 
-await enviormentConfig();
-expressConfig();
-bootstrap();
+enviormentConfig().then(() => {
+    expressConfig();
+    bootstrap();
+});
