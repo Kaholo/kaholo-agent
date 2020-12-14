81ef84fa6eecd0323e2807028ac88186c16592b3
arispen <arispen@gmail.com>
2020-12-14 16:40:53 +0100

    test send to queue

--- tests/unit-tests/amqp.service.test.js
+++ tests/unit-tests/amqp.service.test.js
@@ -1,6 +1,11 @@
+const amqpService = require("../../api/services/amqp.service");
+
 describe("amqp service test", () => {
-  beforeEach(() => {});
   it("should send message to queue", async () => {
-    expect(true).toBeTruthy();
+      // sendToQueue(queue, vhost, message, opts = {})
+    const spy = jest.spyOn(amqpService, "sendToQueue");
+    const message = amqpService.sendToQueue.sendToQueue(queue, vhost, message, opts = {});
+    expect(spy).toHaveBeenCalled();
+    spy.mockRestore();
   });
 });
