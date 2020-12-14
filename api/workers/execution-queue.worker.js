06496efbe654527830d35a3441b92dd9d5484d60
Krzysztof Buchacz <k.buchacz@gmail.com>
2020-12-14 10:01:30 +0100

    Fix results

--- api/workers/execution-queue.worker.js
+++ api/workers/execution-queue.worker.js
@@ -1,10 +1,10 @@
-const { consumeQueue, sendToQueue, VHOST_ACTIONS } = require("../services/amqp.service");
+const { consumeQueue, sendToQueue, VHOST_ACTIONS, VHOST_RESULTS } = require("../services/amqp.service");
 const executionManager = require("../execution-manager");
 
 
 async function processRequest(executionRequest) {
     const executionResult = await executionManager.execute(executionRequest);
-    await sendToQueue(process.env.AMQP_RESULT_QUEUE, Buffer.from(executionResult));
+    await sendToQueue(process.env.AMQP_RESULT_QUEUE, VHOST_RESULTS, Buffer.from(JSON.stringify(executionResult)));
 }
 
 module.exports = async () => {
