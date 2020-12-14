cbcf4fb671ae33a25418c85123ac70b9505d63c9
kbuchacz <kbuchacz@users.noreply.github.com>
2020-12-14 16:33:37 +0100

    task/KAH-940 (Create RabbitMQ connection) (#56)
    * Refactor to new, start workers by default to process items
    
    * Connection done
    
    * Fix results
    
    * Refactor to class and add unsubscribe option
    
    * Fix bug
    
    * Fix endline
    
    * Move to private fields
    
    * Move to private fields

---
+++ api/services/amqp.service.js
@@ -0,0 +1,86 @@
+const { connect } = require("amqplib");
+const fs = require("fs");
+
+class AmqpService {
+    VHOST_RESULTS = "results";
+    VHOST_ACTIONS = "actions";
+
+    #connection = {};
+    #channel = {};
+    #consumerTag = {};
+
+    #opts = {
+        rejectUnauthorized: false,
+        cert: fs.readFileSync("./config/certs/client_certificate.pem"),
+        key: fs.readFileSync("./config/certs/client_key.pem")
+    };
+
+    constructor() {}
+    
+    async #connectToAMQP(vhost) {
+        const connection = await connect(
+            `amqps://${process.env.AMQP_USER}:${process.env.AMQP_PASSWORD}@${process.env.AMQP_HOST}:${process.env.AMQP_PORT}/${vhost}`,
+            this.#opts
+        );
+        if (connection) {
+            this.#connection[vhost] = connection;
+        }
+        return connection;
+    }
+
+    async #createChannel(vhost) {
+        const channel = await this.#connection[vhost].createChannel();
+        if (channel) {
+            this.#channel[vhost] = channel;
+        }
+        return channel;
+    }
+
+    async #amqpConnect(vhost) {
+        const connection = await this.#connectToAMQP(vhost);
+        if (connection) {
+            const channel = await this.#createChannel(vhost);
+            if (channel) {
+                return channel;
+            }
+            throw new Error("Could not create AMQP channel!");
+        } else {
+            throw new Error("Could not connect to AMQP queue!");
+        }
+    }
+
+    async #checkIfQueueExists(queue, vhost) {
+        return this.#channel[vhost].checkQueue(queue);
+    }
+
+    async connectToActions() {
+        return this.#amqpConnect(this.VHOST_ACTIONS);
+    }
+
+    async connectToResults() {
+        return this.#amqpConnect(this.VHOST_RESULTS);
+    }
+
+    async unsubscribe(vhost, queue) {
+        if (!this.#consumerTag[vhost + queue]) {
+            throw new Error("Cannot unsubscribe from queue with undefined consumer");
+        }
+        return this.#channel[vhost].cancel(this.#consumerTag[vhost + queue]);
+    }
+
+    async sendToQueue(queue, vhost, message, opts = {}) {
+        return this.#channel[vhost].sendToQueue(queue, message, opts);
+    }
+
+    async consumeQueue(queue, vhost, cb, opts = []) {
+        const exists = await this.#checkIfQueueExists(queue, vhost);
+        if (exists) {
+            this.#consumerTag[vhost + queue] = await this.#channel[vhost].consumeQueue(queue, cb, opts);
+            return this.#consumerTag[vhost + queue];
+        }
+        throw new Error(`Agent queue ${queue} does not exist!`);
+    }
+
+};
+
+module.exports = new AmqpService();
--- utils/bootstrap.js
+++ utils/bootstrap.js
@@ -2,17 +2,26 @@ const logger = require("../api/services/logger");
 
 const pluginsService = require("../api/services/plugins.service");
 const socketService = require("../api/services/socket.service");
+const amqpService = require("../api/services/amqp.service");
 const register = require("../utils/register");
 
-module.exports = () => {
+const executionQueueWorker = require("../api/workers/execution-queue.worker");
+
+module.exports = async () => {
   logger.info("Loading plugins modules...");
   pluginsService.loadAllInsalledPlugins().catch(console.error);
   logger.info("Finish loading plugins");
   logger.info("Sending key to server");
-  register
-    .registerAgent()
-    .then(() => {
-      socketService.subscribeToSocket();
-    })
-    .catch(() => {});
+  await register.registerAgent();
+  logger.info("Agent registered");
+  logger.info("Connecting to AMQP");
+  await amqpService.connectToResults();
+  logger.info("Connected to results queue");
+  await amqpService.connectToActions();
+  logger.info("Connected to actions queue");
+  logger.info("Subscribing to websocket");
+  socketService.subscribeToSocket();
+  logger.info("Websocket connction estabilished");
+  await executionQueueWorker();
+  logger.info("Started processing execution queue");
 };
