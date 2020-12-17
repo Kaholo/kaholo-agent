4108572533f4e1c080a8c3695d069570c6c490f4
Chris Bernat <bernat.christopher@gmail.com>
2020-12-17 13:24:16 +0100

    Adjust amqp to Big Bird tests

--- api/workers/execution-queue.worker.js
+++ api/workers/execution-queue.worker.js
@@ -1,12 +1,23 @@
 const amqplib = require("../services/amqp.service");
 const executionManager = require("../execution-manager");
 
-
 async function processRequest(executionRequest) {
-    const executionResult = await executionManager.execute(executionRequest);
-    await amqplib.sendToQueue(process.env.AMQP_RESULT_QUEUE, amqplib.VHOST_RESULTS, Buffer.from(JSON.stringify(executionResult)));
+  const executionResult = await executionManager.execute(executionRequest);
+  await amqplib.sendToQueue(
+    process.env.AMQP_RESULT_QUEUE,
+    amqplib.VHOST_RESULTS,
+    Buffer.from(JSON.stringify(executionResult))
+  );
 }
 
 module.exports = async () => {
-    return amqplib.consumeQueue(`${process.env.AGENT_KEY}-queue`, amqplib.VHOST_ACTIONS, processRequest);
-}
+  await amqplib.assertQueue(
+    amqplib.VHOST_ACTIONS,
+    `${process.env.AGENT_KEY}-queue`
+  );
+  return amqplib.consumeQueue(
+    `${process.env.AGENT_KEY}-queue`,
+    amqplib.VHOST_ACTIONS,
+    processRequest
+  );
+};
